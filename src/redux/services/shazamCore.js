e7510814cdfa8a1bd0c71d4088874a6462a0829a
Sattvik Palta <sattvikpalta@gmail.com>
2023-03-20 23:45:48 -0400

    Integrates with the Shazam Core API

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "57d04dc191msh371edc5a3acfff3p1b56d1jsn9d7506442ca9"
+      );
+
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
