6c67e8b0f892a38b1e36e0bb167ecb59f8fb6b6a
Sattvik Palta <sattvikpalta@gmail.com>
2023-03-27 20:54:00 -0400

    Adds genre switching

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,6 +16,9 @@ export const shazamCoreApi = createApi({
 
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/v1/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/v1/charts/genre-world?genre_code=${genre}`,
+    }),
     getSongDetails: builder.query({
       query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`,
     }),
@@ -33,6 +36,7 @@ export const shazamCoreApi = createApi({
 
 export const {
   useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
